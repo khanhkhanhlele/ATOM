1f8e77ac08e08c16e2e52679a8e43128aba7daec
Lê Nam Khánh <namkhanh2172@gmail.com>
2023-05-05 14:38:22 +0700

    a

---
+++ train_cf10.sh
@@ -0,0 +1,20 @@
+python train_dcfens_cifar10_CI.py \
+--dataset cifar10 \
+--data_path Datasets/CIFAR10 \
+--num_task 6 \
+--first_task_cls 0 \
+--model resnet18 \
+--num_member 2 \
+--num_bases 48 \
+--init_with_pre \
+--train_batch 32 \
+--test_batch 128 \
+--lr 0.01 \
+--wd 0 \
+--total_epoch 10 \
+--lr_schedule 75-90 \
+--lr_sub 0.01 \
+--wd_sub 0 \
+--total_epoch_sub 10 \
+--lr_schedule_sub 75-90 \
+--gpu 0 \
